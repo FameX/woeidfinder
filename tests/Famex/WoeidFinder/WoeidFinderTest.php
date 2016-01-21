080e59882594035220b5e328face0f8965077399
Dominik Schwind <dschwind@lostfocus.de>
2016-01-21 15:52:14 +0100

    Well, duh.

--- tests/Famex/WoeidFinder/WoeidFinderTest.php
+++ tests/Famex/WoeidFinder/WoeidFinderTest.php
@@ -26,7 +26,7 @@ class WoeidFinderTest extends PHPUnit_Framework_TestCase
 			$place = $this->woeidFinder->getPlace(3.1578500,101.7116500);
 			$this->assertInstanceOf('Famex\WoeidFinder\Place\Place',$place,"Did not return a place object");
 			$this->assertInstanceOf('Famex\WoeidFinder\Place\WoEID',$place->getWoeid(),"The place object does not have a woeid object");
-			$this->assertEquals('28347326',$place->getWoeid()->woeid,'The place object has a wrong woeid for this lat/long');
+			$this->assertEquals('1154781',$place->getWoeid()->woeid,'The place object has a wrong woeid for this lat/long');
 
 			$this->setExpectedException('Exception');
 			$place = $this->woeidFinder->getPlace(0,0);
